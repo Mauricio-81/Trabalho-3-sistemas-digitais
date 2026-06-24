6a6ec5eac5b58aa2b2068adc883a529cdfb1ac68
lucasmachado7777 <lucas-lgam@hotmail.com>
2026-06-24 11:42:31 -0300

    Update top_fpga.sv

--- top_fpga.sv
+++ top_fpga.sv
@@ -11,7 +11,6 @@ module top_fpga (
     output logic [7:0] AN
 );
 
-    // Sinais internos
     logic rst_n;
     logic ready_tb;
     logic start_tb;
@@ -25,8 +24,6 @@ module top_fpga (
 
     logic locked;
 
-    // Geração dos clocks pelo Clocking Wizard
-    // O nome "clk_wiz_0" pode mudar dependendo do nome criado no Vivado.
     clk_wiz_0 clk_gen (
         .clk_in1  (clock),
         .reset    (reset),
@@ -40,19 +37,14 @@ module top_fpga (
         .locked   (locked)
     );
 
-    // Reset interno ativo em nível baixo
-    // O projeto só sai do reset quando o Clocking Wizard estiver travado
     assign rst_n = locked & ~reset;
 
-    // Botão usado para iniciar a operação
-    assign start_tb = ssl;
-
-    // Switches usados para selecionar o sensor
+    assign start_tb  = ssl;
     assign reg_id_tb = SW[1:0];
 
-    // Instância do top original do T3
     top u_top (
         .rst        (rst_n),
+
         .clk_15mhz  (clk_15mhz),
         .clk_25mhz  (clk_25mhz),
         .clk_40mhz  (clk_40mhz),
@@ -64,14 +56,12 @@ module top_fpga (
         .reg_id_tb (reg_id_tb)
     );
 
-    // LEDs para observação na placa
     assign LED[0]    = ready_tb;
     assign LED[1]    = start_tb;
     assign LED[3:2]  = reg_id_tb;
     assign LED[4]    = ssr;
     assign LED[15:5] = SW[15:5];
 
-    // Display de 7 segmentos desligado
     assign DDP = 8'hFF;
     assign AN  = 8'hFF;
 
